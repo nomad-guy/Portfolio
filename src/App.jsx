1e5dae3cac97794354bb52628029318dfef0de0a
GuruMachanica <mdhuzaifa00786@gmail.com>
2026-09-07 10:23:23 +0530

    Initial commit: Persona3 portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import menuVideo from './assets/Mainn.mp4'
+import main2 from './assets/main2.mp4'
+import P3Menu from './P3Menu'
+import VideoPage from './VideoPage'
+import ResumePage from './ResumePage'
+import PageTransition from './PageTransition'
+import Socials from './Socials'
+import AboutMe from './AboutMe'
+import SideProjects from './SideProjects'
+import main3 from './assets/main3.mp4'
+import './App.css'
+
+function MenuScreen() {
+  const navigate = useNavigate()
+  return (
+    <div id="menu-screen">
+      <video src={menuVideo} autoPlay loop muted playsInline />
+      <P3Menu onNavigate={(page) => navigate(`/${page}`)} />
+    </div>
+  )
+}
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={
+          <PageTransition><MenuScreen /></PageTransition>
+        } />
+        <Route path="/about" element={
+          <PageTransition variant="about"><AboutMe /></PageTransition>
+        } />
+        <Route path="/resume" element={
+          <PageTransition><ResumePage src={main2} /></PageTransition>
+        } />
+        <Route path="/socials" element={
+          <PageTransition variant="socials"><Socials /></PageTransition>
+        } />
+        <Route path="/sideproj" element={
+          <PageTransition><SideProjects src={main3} /></PageTransition>
+        } />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+export default function App() {
+  return <AnimatedRoutes />
+}
